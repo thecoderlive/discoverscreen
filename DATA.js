25abf65d21f66d6540e2ff214bcc5dd477503ca3
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-13 14:03:21 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,89 @@
+export const item = {
+    "id": "06123ee3-4523-4572-a84f-ba073b7f8f6b",
+    "discovery_list": [
+        {
+            "id": "1aaa49c5-736a-40af-9689-76e79abf3a8e",
+            "discovery_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png",
+            "founder_name": "experience",
+            "discovery_name": "Ahead risk bit majority window note environment. Budget organization nor manage traditional.",
+            "lock_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png",
+            "about_discovery": "Fact head building appear send. Measure prepare ability sister staff describe. Else thousand mention center."
+        },
+        {
+            "id": "863709d0-bbc1-420d-8508-246d4741e86a",
+            "discovery_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png",
+            "founder_name": "song",
+            "discovery_name": "Wife southern determine strategy. Begin but physical result score magazine control.",
+            "lock_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png",
+            "about_discovery": "History north hard imagine man wish seek. Around site north forward."
+        },
+        {
+            "id": "1c1f0944-a27b-4543-bc4e-b0ac010b4216",
+            "discovery_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/202.png",
+            "founder_name": "me",
+            "discovery_name": "Player hear institution large final the operation business. Believe enough later lay who expect seem. Its note true significant suggest accept community.",
+            "lock_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png",
+            "about_discovery": "Subject those bank so right. None clearly focus. Research PM age laugh wrong word ball."
+        },
+        {
+            "id": "f9a94164-e6ed-404b-afb1-10dbc2307dbd",
+            "discovery_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png",
+            "founder_name": "send",
+            "discovery_name": "Movie body character very name factor somebody. Final but father statement. Thousand sort matter middle.",
+            "lock_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/27.png",
+            "about_discovery": "Knowledge second in want economy off task. Road card exist provide. Teacher college war field you prove deal."
+        },
+        {
+            "id": "7930bdf5-faba-4311-9f41-a092dc0c4ff9",
+            "discovery_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/362.png",
+            "founder_name": "present",
+            "discovery_name": "Character ability season personal military white. Sit situation under experience. Sound its condition major capital stay nature.",
+            "lock_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png",
+            "about_discovery": "Dream decide program science small get. Onto sing view their black."
+        }
+    ],
+    "new_in": "wide",
+    "about_just_in": "Attention book toward list the. Edge because age sound until information.",
+    "newest_list": [
+        {
+            "id": "c0b81005-7a71-4153-a848-751c0bda30f7",
+            "newest_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png",
+            "founder": "store",
+            "newest_name": "Fund business on. Already structure tax begin tax would.",
+            "lock_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png",
+            "about_newest": "Already itself employee best respond sea strategy. Top the feeling tonight."
+        },
+        {
+            "id": "b267f287-5924-4eef-8b52-d19a9e4185d2",
+            "newest_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png",
+            "founder": "ground",
+            "newest_name": "Street customer American person policy summer camera.",
+            "lock_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png",
+            "about_newest": "Notice grow their decade. Plant public phone start. None goal marriage campaign language."
+        },
+        {
+            "id": "61a3c044-8195-4255-b0f2-00bbf564cf1d",
+            "newest_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png",
+            "founder": "attention",
+            "newest_name": "Fund his lose only explain save. Well card whether beat.",
+            "lock_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/171.png",
+            "about_newest": "Trouble example area star girl daughter matter education. Could try also as those. Seat culture reflect significant shake everything article."
+        },
+        {
+            "id": "d8f6e2ab-5dd4-4338-8f4d-240bfb900e2b",
+            "newest_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png",
+            "founder": "series",
+            "newest_name": "Western something prepare music. Decide management simply new. Center education anyone power explain behind defense.",
+            "lock_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png",
+            "about_newest": "Already itself employee best respond sea strategy. Top the feeling tonight."
+        },
+        {
+            "id": "4fff6866-78fb-4887-b91b-aa56abf790df",
+            "newest_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png",
+            "founder": "manager",
+            "newest_name": "Kind group candidate nor will. Successful we current too write coach seat.",
+            "lock_icon2": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png",
+            "about_newest": "End center hold concern kind rise. Student teacher threat service try together. Go minute be."
+        }
+    ]
+}
